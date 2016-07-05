410dfd829d0d3d3227363d483fbc77aeb2711a20
Chris Flint <flintedc@gmail.com>
2016-07-05 18:58:00 +0100

    updated seeds

--- db/seed.rb
+++ db/seed.rb
@@ -13,35 +13,75 @@ Knight.delete_all
 Horse.delete_all
 Kingdom.delete_all
 
-@kingdom1= Kingdom.new({'name' => 'kingdom of scotland', 'flag' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Lionrampant.svg/2000px-Lionrampant.svg.png','arms' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Royal_Coat_of_Arms_of_the_Kingdom_of_Scotland.svg/2000px-Royal_Coat_of_Arms_of_the_Kingdom_of_Scotland.svg.png'}).save
-@kingdom2= Kingdom.new({'name' => 'kingdom of england', 'flag' => 'http://www.templateof.com/postpic/2014/07/medieval-england-flag_264431.PNG','arms' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Royal_Coat_of_Arms_of_the_United_Kingdom.svg/2000px-Royal_Coat_of_Arms_of_the_United_Kingdom.svg.png'}).save
-@kingdom3= Kingdom.new({'name' => 'kingdom of france', 'flag' => 'https://upload.wikimedia.org/wikipedia/en/1/1f/Flag_of_medieval_France.png','arms' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Grand_Royal_Coat_of_Arms_of_France_%26_Navarre.svg/2000px-Grand_Royal_Coat_of_Arms_of_France_%26_Navarre.svg.png'}).save
+@kingdom1= Kingdom.new({'name' => 'kingdom of scotland', 'flag' => '/flags/scotland.jpg','arms' => '/arms/'}).save
+@kingdom2= Kingdom.new({'name' => 'kingdom of england', 'flag' => '/flags/england.png','arms' => '/arms/'}).save
+@kingdom3= Kingdom.new({'name' => 'frankish empire', 'flag' => '/flags/france.jpg','arms' => '/arms/'}).save
+@kingdom4= Kingdom.new({'name' => 'lordship of ireland', 'flag' => '/flags/ireland.png','arms' => '/arms/'}).save
+@kingdom5= Kingdom.new({'name' => 'kingdom of wales', 'flag' => '/flags/wales.jpg','arms' => '/arms/'}).save
+@kingdom6= Kingdom.new({'name' => 'holy roman empire', 'flag' => '/flags/holyroman.png','arms' => '/arms/'}).save
+@kingdom7= Kingdom.new({'name' => 'byzantine empire', 'flag' => '/flags/byzantine.png','arms' => '/arms/'}).save
+@kingdom8= Kingdom.new({'name' => 'duchy of burgundy', 'flag' => '/flags/burgundy.png','arms' => '/arms/'}).save
+@kingdom9= Kingdom.new({'name' => 'kingdom of aragon', 'flag' => '/flags/aragon.svg','arms' => '/arms/'}).save
+@kingdom10= Kingdom.new({'name' => 'kingdom of sicily', 'flag' => '/flags/sicily.png','arms' => '/arms/'}).save
+@kingdom11= Kingdom.new({'name' => 'kingdom of hungary', 'flag' => '/flags/hungary.png','arms' => '/arms/'}).save
+@kingdom12= Kingdom.new({'name' => 'kingdom of poland', 'flag' => '/flags/poland.png','arms' => '/arms/'}).save
+@kingdom13= Kingdom.new({'name' => 'kingdom of bulgaria', 'flag' => '/flags/bulgaria.png','arms' => '/arms/'}).save
+@kingdom14= Kingdom.new({'name' => 'kingdom of naples', 'flag' => '/flags/naples.png','arms' => '/arms/'}).save
 
-@horse1= Horse.new({'name'=>'Horsey Mchorseface', 'img' => 'http://www.isoladellecolombe.it/wp-content/uploads/2016/05/frison-black.jpg'}).save
-@horse2= Horse.new({'name'=>'Redrum', 'img' => 'http://isyourgirlfriendahorse.com/carousel-horse-1.jpg'}).save
-@horse3= Horse.new({'name'=>'Horse I win', 'img' => 'http://media1.santabanta.com/full1/Animals/Horses/horses-82a.jpg'}).save
-@horse4= Horse.new({'name'=>'Racheal', 'img' => 'https://s-media-cache-ak0.pinimg.com/736x/bd/a8/b1/bda8b1a2fbaac4cdfc4de55ee0f8bf3d.jpg'}).save
 
-@knight1= Knight.new({'name' => 'Sir Reginald', 'nation_id' => @kingdom1.id,'img' => 'http://www.3dtotal.com/admin/new_cropper/tutorial_content_images/797_tid_thelastknight.jpg', 'wealth' => 500}).save
-@knight2= Knight.new({'name' => 'Sir Percival', 'nation_id' => @kingdom2.id,'img' => 'http://p2.storage.canalblog.com/24/16/577050/54588570.jpg', 'wealth' => 500}).save
-@knight3= Knight.new({'name' => 'Sir Robin', 'nation_id' => @kingdom3.id,'img' => 'https://s-media-cache-ak0.pinimg.com/236x/08/fb/73/08fb73851b6c0bacbfed0599850a9fa0.jpg', 'wealth' => 500}).save
-@knight4= Knight.new({'name' => 'Sir Galahad', 'nation_id' => @kingdom1.id,'img' => 'https://s-media-cache-ak0.pinimg.com/736x/28/1f/81/281f81f43d14cc46b6ba1326f031a6e5.jpg', 'wealth' => 500}).save
-@knight5= Knight.new({'name' => 'Sir Artod', 'nation_id' => @kingdom1.id,'img' => 'http://1.bp.blogspot.com/_o8EbVx1uV6c/S_04U5HCW1I/AAAAAAAAA7I/wEKhVoAMcTM/s1600/20080715174808_medieval_punk.jpg', 'wealth' => 500}).save
-@knight6= Knight.new({'name' => 'Sir Baran', 'nation_id' => @kingdom2.id,'img' => 'https://s-media-cache-ak0.pinimg.com/236x/e0/75/be/e075bebc855a639ff3703dfff61820c7.jpg', 'wealth' => 500}).save
-@knight7= Knight.new({'name' => 'Sir Tim', 'nation_id' => @kingdom2.id,'img' => 'https://pixabay.com/static/uploads/photo/2013/06/07/22/33/knight-122910_960_720.jpg', 'wealth' => 500}).save
-@knight8= Knight.new({'name' => 'Sir Simon', 'nation_id' => @kingdom1.id,'img' => 'https://s-media-cache-ak0.pinimg.com/236x/f0/e7/14/f0e71430b835e8950da0e213e7548190.jpg', 'wealth' => 500}).save
+@horse1= Horse.new({'name'=>'Horsey Mchorseface', 'img' => '/horses/horse1.jpg'}).save
+@horse2= Horse.new({'name'=>'Chuck', 'img' => '/horses/horse2.jpg'}).save
+@horse3= Horse.new({'name'=>'Horace', 'img' => '/horses/horse6.jpg'}).save
+@horse4= Horse.new({'name'=>'Trotalot', 'img' => '/horses/horse7.jpg'}).save
+@horse5= Horse.new({'name'=>'Hamburger', 'img' => '/horses/horse4.jpg'}).save
+@horse6= Horse.new({'name'=>'Red Rum', 'img' => '/horses/horse8.jpg'}).save
+@horse7= Horse.new({'name'=>'Canterbury', 'img' => '/horses/horse3.jpg'}).save
+@horse8= Horse.new({'name'=>'Mr Jones', 'img' => '/horses/horse5.jpg'}).save
+
+@knight1= Knight.new({'name' => 'Sir Jock', 'nation_id' => @kingdom1.id,'img' => '/knights/knight1.jpg', 'wealth' => 0}).save
+@knight2= Knight.new({'name' => 'Sir Tim', 'nation_id' => @kingdom2.id,'img' => '/knights/knight2.jpg', 'wealth' => 0}).save
+@knight3= Knight.new({'name' => 'Sir Pierre', 'nation_id' => @kingdom3.id,'img' => '/knights/knight3.jpg', 'wealth' => 0}).save
+@knight4= Knight.new({'name' => 'Sir Connor', 'nation_id' => @kingdom4.id,'img' => '/knights/knight4.jpg', 'wealth' => 0}).save
+@knight5= Knight.new({'name' => 'Sir Hugh', 'nation_id' => @kingdom5.id,'img' => '/knights/knight5.jpg', 'wealth' => 0}).save
+@knight6= Knight.new({'name' => 'Sir Rico', 'nation_id' => @kingdom6.id,'img' => '/knights/knight6.jpg', 'wealth' => 0}).save
+@knight7= Knight.new({'name' => 'Sir Constantine', 'nation_id' => @kingdom7.id,'img' => '/knights/knight7.jpg', 'wealth' => 0}).save
+@knight8= Knight.new({'name' => 'Sir Paul', 'nation_id' => @kingdom8.id,'img' => '/knights/knight8.jpg', 'wealth' => 0}).save
+@knight9= Knight.new({'name' => 'Sir Reginald', 'nation_id' => @kingdom9.id,'img' => '/knights/knight9.jpg', 'wealth' => 0}).save
+@knight10= Knight.new({'name' => 'Sir Cumference', 'nation_id' => @kingdom10.id,'img' => '/knights/knight10.jpg', 'wealth' => 0}).save
+@knight11= Knight.new({'name' => 'Sir Zsolt', 'nation_id' => @kingdom11.id,'img' => '/knights/knight11.jpg', 'wealth' => 0}).save
+@knight12= Knight.new({'name' => 'Sir Cuthbert', 'nation_id' => @kingdom12.id,'img' => '/knights/knight12.jpg', 'wealth' => 0}).save
+@knight13= Knight.new({'name' => 'Sir Arthur', 'nation_id' => @kingdom13.id,'img' => '/knights/knight13.jpg', 'wealth' => 0}).save
+@knight14= Knight.new({'name' => 'Sir Thomas', 'nation_id' => @kingdom14.id,'img' => '/knights/knight14.jpg', 'wealth' => 0}).save
+@knight15= Knight.new({'name' => 'Sir Bruce', 'nation_id' => @kingdom1.id,'img' => '/knights/knight15.jpg', 'wealth' => 0}).save
+@knight16= Knight.new({'name' => 'Sir Cameron', 'nation_id' => @kingdom2.id,'img' => '/knights/knight16.jpg', 'wealth' => 0}).save
+@knight17= Knight.new({'name' => 'Sir Alan', 'nation_id' => @kingdom3.id,'img' => '/knights/knight17.jpg', 'wealth' => 0}).save
+@knight18= Knight.new({'name' => 'Sir Orion', 'nation_id' => @kingdom11.id,'img' => '/knights/knight18.jpg', 'wealth' => 0}).save
+@knight19= Knight.new({'name' => 'Sir Stefan', 'nation_id' => @kingdom10.id,'img' => '/knights/knight19.jpg', 'wealth' => 0}).save
+@knight20= Knight.new({'name' => 'Sir Derek', 'nation_id' => @kingdom8.id,'img' => '/knights/knight20.jpg', 'wealth' => 0}).save
+
 
 @list=List.new([@knight2,@knight1,@knight3,@knight4])
 @list.save
 
-@event1=Event.new({'name' => 'fast joust','type' => 'jousting','family'=>'mounted','max' => 5, 'img' => '/event/joust.png','prize' => 500,'event_date' => '2016-03-30'}).save
-@event2=Event.new({'name' => '200yd archery','type' => 'archery','family'=>'ranged','max' => 5, 'img' => '/event/archery.png','prize' => 200,'event_date' => '2016-05-30'}).save
-@event1=Event.new({'name' => 'Sword & Shield','type' => 'combat on foot','family'=>'On foot','max' => 6, 'img' => '/event/sword.png','prize' => 400,'event_date' => '1216-03-30'}).save
-
-@event1.add_knight(@knight1)
-@event1.add_knight(@knight2)
-@event1.add_knight(@knight3)
-@event1.add_knight(@knight4)
-@event1.run_event
+@event1=Event.new({'name' => 'fast joust','type' => 'joust','family'=>'mounted','max' => 5, 'prize' => 500,'event_date' => '1299-03-30'}).save
+@event2=Event.new({'name' => '200yd archery','type' => 'archery','family'=>'ranged','max' => 5,'prize' => 200,'event_date' => '1299-05-30'}).save
+@event3=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event4=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event5=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event6=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event7=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event8=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event9=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event10=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event11=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event12=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event13=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event14=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event15=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event16=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event17=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event18=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event19=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
+@event20=Event.new({'name' => 'Sword & Shield','type' => 'sword','family'=>'On foot','max' => 6,'prize' => 400,'event_date' => '1299-03-30'}).save
 
 @ranking= Ranking.new()
